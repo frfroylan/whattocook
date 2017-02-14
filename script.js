179a94eced2ed22baeaed2395671388e49a3822a
Froy Terriquez <frfroylan@yahoo.com>
2017-02-13 19:35:05 -0800

    added handlebarjs and updated styles

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
 	var diettext = '';
 	var key = '';
 	var recipes = [];
-	var returnedItems = $('#flex-items');
+	var returnedItems = $('#container');
 	var template = Handlebars.compile($('#recipe-template').html());
-	//Grabs dropdown menu items and updates dropdown button text
+
+	//Updates dropdown button text and color when selection is made
 	$('.dropdown-item').click(function(){
 		if($(this).parent().hasClass('js-health')){
 			healthtext = $(this).text();
@@ -21,6 +22,7 @@ $(document).ready(function(){
 		}
 	});
 
+	//Returns health or diet string and adds necessary text
 	function getHealthString(str, x){
 		if(str === '' || str === 'none')
 			return '';
@@ -32,6 +34,7 @@ $(document).ready(function(){
 			return 'Wrong health/diet restrction';
 	}
 
+	//Adds recipe info to recipes array
 	function addRecipeObj(item, index, array){
 		recipes.push({
 			'label': array[index].recipe.label,
@@ -42,13 +45,7 @@ $(document).ready(function(){
 		});
 	};
 
-	function pushToDom(item, index, array){
-		var singleRecipe = item;
-		for(var x = 0; x < array.length; x++){
-			returnedItems.html(template(singleRecipe));
-		}
-	}
-
+	//When search button is clicked create a new url
 	$('#search').click(function(){
 		var url ='https://api.edamam.com/search?api_id=bc8cbf59&api_key=ecf7fb85c3b186ae3ea428fbc209fa4c';
 		//Gets keyword
@@ -56,15 +53,20 @@ $(document).ready(function(){
 		//Health /Diet
 		healthtext = getHealthString(healthtext, 'h');
 		diettext = getHealthString(diettext, 'd');
+		//URL generated each time a new click is made
 		url = url + key + healthtext + diettext;
+		console.log(url);
 		$.ajax({
 			url: url,
 			method: 'GET',
 		}).done(function(result){
 			//Get the objects that contain the recipes which are under the hits node
 			var data = result.hits;
-			data.forEach(addRecipeObj);
-			recipes.forEach(pushToDom);
+			data.forEach(addRecipeObj); //Performs funciton on each object 
+			returnedItems.html(template(recipes));
+			$('.main-header').css('display','block');
+		}).fail(function(err){
+			alert('Error occured: ', err);
 		})
 	});
 })
